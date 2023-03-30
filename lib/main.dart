a67eb004955bd9140abe4b35a4180fd363f6c11e
sagarck07 <shubhamsagar575@gmail.com>
2023-03-30 16:37:45 +0530

    Day 2 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:e_commerce_flutter/home_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,13 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Material(
-        child: Center(
-          child: Container(
-            child: Text("Welcome"),
-          ),
-        ),
-      ),
+      home: HomePage(),
     );
   }
 }
